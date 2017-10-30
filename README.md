4150d86b80c908480e231c8097fbcfa9ad812967
ZuperKuchen <remibrisset@gmail.com>
2017-10-30 18:26:38 +0100

    Makefile
    fin de session

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 Devoir Maison Complexité et Calculabilité
-BRISSET Rémi & CHAUVEAU Pierre# DMCoCaBrissetChauveau
+BRISSET Rémi & CHAUVEAU Pierre
+
+
+============================================
+==========Compilation & Exécution===========
+
+~$ make        //Pour compiler tout les graphes du repertoire /test
+~$ cd glucose-syrup-4.1/simp/
+~/glucose-syrup-4.1/simp/$ make //Pour compiler glucose
+
+
+Pour executer:
+~$./nom_exe_graphe h  //avec h la hauteur de l'arbre et nom_exe_graphe le nom de du graphe à tester
+
+
+
+============================================
+
+
--- main.c
+++ main.c
@@ -1,9 +1,74 @@
 #include "test/all.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+#define GLUCOSE_EXE "./glucose-syrup-4.1/simp/glucose"
+#define ARG 0
+#define FIN_LIGNE "\n"
 
+void usage(int err){
+  switch(err){
+  case ARG:
+    printf("usage : ./nom_exe h avec h > 0 \n");
+    break;
+  default:
+    printf("Appel usage non reconnue \n");
+    break;
+  }
+}
 
 int main(int argc, char **argv){
+
+  //TO REMOVE
+  printf("argc: %d \n" ,argc);
+  for (int i=0; i<argc; i++){
+    printf("argv[%d]:%d \n" ,i,atoi(argv[i]));
+  }
+  //
+
+  
+  if (argc != 2) { //On verifie si l'utilisateur à bien passé la hauteur en paramètre
+    usage(ARG);
+    return EXIT_FAILURE;
+  }
+  int height = atoi(argv[1]); //On recupere la hauteur
+  if (height <= 0){ //On verifie que h > 0 
+    usage(ARG);
+    return EXIT_FAILURE;
+  }
+  int nbVer = orderG(); //nombre de sommets n
+  //int nbEdg = sizeG();  //nombre d'aretes m
+  int nbVar = nbVer * height ; //nombre de variables Xv,h
+  int nbClauses = 0;
+
+  
+  //
+  //ECRIRE LE FICHIER//
+  const char* file_name = "file.txt";
+  FILE* file = fopen(file_name, "w");
+  char *buffer = "p cnp";
+  fwrite(buffer, sizeof(char), strlen(buffer),file);
+  fwrite(&nbVar, sizeof(char), 1,file);
+  fclose(file);
+  //
+  //
+
+  char *file_name_test = "test.txt";
+  FILE* file_test = fopen(file_name_test, "w");
+
+
+  
+  //
+  //LE PASSER DANS GLUCOSE
+  //Penser à gérer le cas ou glucose n'est pas compilé
+  //printf("test 1\n");
+  execl(GLUCOSE_EXE, GLUCOSE_EXE, file_name_test, (const char*) NULL);
+  //printf("test 2\n");
+  //
+  //
+  
+  
   return 1;
 }
