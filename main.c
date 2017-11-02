dd9f2b52b7cc13d218b666e74b9db7a0e63fbcf4
ZuperKuchen <remibrisset@gmail.com>
2017-11-02 14:14:38 +0100

    ajout condition 2 3

--- main.c
+++ main.c
@@ -41,7 +41,7 @@ int main(int argc, char **argv){
   int nbVer = orderG(); //nombre de sommets n
   //int nbEdg = sizeG();  //nombre d'aretes m
   int nbClauses = 0;
-  int nbVar = nbVer * height ; //nombre de variables Xv,h
+  int nbVar = nbVer * (height+1) ; //nombre de variables Xv,h
   int nom_var = 1;
 
   //INITIALISATION VAR//
@@ -56,14 +56,16 @@ int main(int argc, char **argv){
   const char* file_name = "file.txt";
   FILE* file = fopen(file_name, "w");
   char *buffer = malloc(sizeof(char) * (nbVar*2+2)); //Taille maximal d'une clause: 2*nb variables (variables + espaces) + 2 ( 0 de fin de ligne et \n)
+  //*1ere ligne*//
   sprintf(buffer,"p cnf ");
   fwrite(buffer, sizeof(char), strlen(buffer), file);
   sprintf(buffer,"%d",nbVar);
   fwrite(buffer, sizeof(char), strlen(buffer), file);
-  //  fwrite(&nbVar, sizeof(char), 1,file);
   sprintf(buffer,"\n");
-    fwrite(buffer, sizeof(char), strlen(buffer), file);
+  fwrite(buffer, sizeof(char), strlen(buffer), file);
+
   //*Condition 1*//
+  //Pour tout v dans V Il existe au moins un h tel que Xvh vrai 
   for(int i=0; i < nbVer; i++){
     for(int j=0; j <= height; j++){
       sprintf(buffer, "%d ", matrice_var[i][j]);
@@ -73,9 +75,48 @@ int main(int argc, char **argv){
     fwrite(buffer, sizeof(char), strlen(buffer), file);
     nbClauses++;
   }
+  //Il en existe au plus un 
+  for(int i=0; i < nbVer; i++){
+    for(int j=0; j < height; j++){
+      for(int l=j+1; l <=height; l++){
+	sprintf(buffer,"-%d -%d 0\n", matrice_var[i][j], matrice_var[i][l]);
+	fwrite(buffer, sizeof(char), strlen(buffer), file);
+	nbClauses++;
+      }
+    }
+  }  
+  //*Condition 2*//
+  //Il existe au moins un xv0 vrai
+  for (int i=0; i < nbVer; i++){
+    sprintf(buffer,"%d ",matrice_var[i][0]);
+    fwrite(buffer, sizeof(char), strlen(buffer), file);
+  }
+  sprintf(buffer,"0\n");
+  fwrite(buffer, sizeof(char), strlen(buffer), file);
+  nbClauses++;
+  //Il en existe au  plus un
+  for (int i=0; i < nbVer - 1; i++){
+    for (int j=i+1; j < nbVer; j++){
+      sprintf(buffer,"-%d -%d 0\n",matrice_var[i][0], matrice_var[j][0]);
+      fwrite(buffer, sizeof(char), strlen(buffer), file);
+      nbClauses++;
+    }
+  }
 
+  //*Condition 3*//
+  //Il existe au moins un sommet v tel que Xvk vrai
+  for(int i=0; i < nbVer; i++){
+    sprintf(buffer,"%d ", matrice_var[i][height]);
+    fwrite(buffer, sizeof(char), strlen(buffer), file);
+  }
+  sprintf(buffer,"0\n");
+  fwrite(buffer, sizeof(char), strlen(buffer), file);
+  nbClauses++;
 
+  //*Condition 4*//
+  //TO DO//
 
+ 
 
 
 
@@ -83,20 +124,15 @@ int main(int argc, char **argv){
   fclose(file);
   //
   //
-  /*
-  char *file_name_out = "res.txt";
-  FILE* file_res = fopen(file_name_out, "w");
-  fclose(file_res);
-  */
-
-  char *file_name_test = "test.txt";
+  
+  char *file_res_name = "res.txt";
+  FILE* file_res = fopen(file_res_name, "w");
+  //fclose(file_res);
   
   //
   //LE PASSER DANS GLUCOSE
   //Penser à gérer le cas ou glucose n'est pas compilé
-  //printf("test 1\n");
-  //execl(GLUCOSE_EXE, GLUCOSE_EXE, file_name_test, (const char*) NULL);
-  //printf("test 2\n");
+  execl(GLUCOSE_EXE, GLUCOSE_EXE, file, file_res, (const char*) NULL);
   //
   //
   /*
@@ -106,4 +142,4 @@ int main(int argc, char **argv){
   
   
   return 1;
-}
+  }
